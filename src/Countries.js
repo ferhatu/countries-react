36063131b6cb0234e8b3279c7dd7919bd8849276
ferhatu <ferhat_2001@yahoo.com>
2020-05-30 19:39:57 +0100

    update

---
+++ src/Countries.js
@@ -0,0 +1,22 @@
+import React from "react";
+import data from "./data/countriesAll.json";
+
+const Countries = () => {
+  return (
+    <div className="countries-container">
+      {data.map((country) => {
+        return (
+          <div className="country">
+            <img className="image" src={country.flag} alt="country flag" />
+            <h1 className="country-name">{country.name}</h1>
+
+            <p>Population: {country.population}</p>
+            <p>Region: {country.region}</p>
+            <p>Capital: {country.capital}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Countries;
